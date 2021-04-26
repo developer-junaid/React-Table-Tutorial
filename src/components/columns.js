180b7fa0d82eaee26c654a461f3440269b80f23e
developer-junaid <qjunaid623@gmail.com>
2021-04-26 15:13:58 +0500

    Step 1 and 2 Complete (Defined Data, Defined columns)
    NEXT: Step 3 (Create a Table Instance)

---
+++ src/components/columns.js
@@ -0,0 +1,27 @@
+// Columns
+export const COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "First Name",
+    accessor: "first_name",
+  },
+  {
+    Header: "Last Name",
+    accessor: "last_name",
+  },
+  {
+    Header: "Date of Birth",
+    accessor: "date_of_birth",
+  },
+  {
+    Header: "Country",
+    accessor: "country",
+  },
+  {
+    Header: "Phone",
+    accessor: "phone",
+  },
+];
