3a97ed8c8932fd771949759b0b1e6118eb6a6f85
developer-junaid <qjunaid623@gmail.com>
2021-04-27 06:10:31 +0500

    Debouncing (delaying filtering start)
    NEXT: Pagination

--- src/components/FilteringTable/ColumnFilter.js
+++ src/components/FilteringTable/ColumnFilter.js
@@ -4,7 +4,7 @@ export const ColumnFilter = ({ column }) => {
   const { filterValue, setFilter } = column;
   return (
     <span>
-      Search:{" "}
+      Column Search:{" "}
       <input
         value={filterValue || ""}
         onChange={(e) => setFilter(e.target.value)}
--- src/components/FilteringTable/GlobalFilter.js
+++ src/components/FilteringTable/GlobalFilter.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAsyncDebounce } from "react-table";
 
 export const GlobalFilter = ({ filter, setFilter }) => {
+  // Debouncing
+  const [value, setValue] = useState(filter);
+
+  const onChange = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 1000);
+
+  // Return
   return (
     <span>
       Search:{" "}
-      <input value={filter || ""} onChange={(e) => setFilter(e.target.value)} />
+      <input
+        value={value || ""}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+      />
     </span>
   );
 };
